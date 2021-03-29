8ca51d33d3a15912e1977d432023f5c7a60907ce
Riddhi Patel <65072577+RiddhiPatel1591@users.noreply.github.com>
2021-03-29 21:38:46 +0530

    Delete BE Website directory

--- BE Website/request.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'departure_date_time':1/10/2018 10:00:00, 'origin':'ATL', 'destination':'SEA'})
-
-print(r.json())
-
-
